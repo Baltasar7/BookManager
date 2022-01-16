6dab7aa6a6ca622694387457e3d39745d1be8ca8
ohno.k <ohno.k@example.com>
2022-01-16 20:18:22 +0900

    Update psql schema, data file.

--- src/main/resources/data_psql.sql
+++ src/main/resources/data_psql.sql
@@ -1,9 +1,9 @@
-INSERT INTO m_user (user_id, password, user_name, department, role)
-VALUES('123456', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '白洲次郎', '東京営業所', 'ROLE_GENERAL');
+INSERT INTO m_user (user_id, password, user_name, mail_addr, department, role)
+VALUES('123456', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '白洲次郎', 'example1@gmail.com.test', '東京営業所', 'ROLE_GENERAL');
 /* VALUES('123456', 'password', '白洲次郎', '東京営業所', 'ROLE_GENERAL'); */
 
-INSERT INTO m_user (user_id, password, user_name, department, role)
-VALUES('112233', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '吉田茂', '福岡営業所', 'ROLE_ADMIN');
+INSERT INTO m_user (user_id, password, user_name, mail_addr, department, role)
+VALUES('112233', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '吉田茂', 'example2@gmail.com.test','福岡営業所', 'ROLE_ADMIN');
 /* VALUES('112233', 'passpass', '吉田茂', '福岡営業所', 'ROLE_ADMIN'); */
 
 
